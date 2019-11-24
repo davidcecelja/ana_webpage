27f3bb5cbaaae102936d0cc10ad42035fe318b6e
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2019-11-24 22:03:52 +0100

    gulp works

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,88 @@
-var gulp       = require('gulp'),
-    sass       = require('gulp-sass'),
-    minify_css = require('gulp-minify-css'),
-    notify     = require('gulp-notify'),
-    autoprefix = require('gulp-autoprefixer'),
-    concat     = require('gulp-concat'),
-    uglify     = require('gulp-uglify'),
-    rename     = require('gulp-rename');
+const
+    // dev mode
+    devBuild = true,
+    // modules
+    gulp = require('gulp'),
+    newer = require('gulp-newer'),
+    noop = require('gulp-noop'),
+    htmlclean = require('gulp-htmlclean'),
+    sync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    terser = require('gulp-terser'),
+    // paths
+    src = 'src',
+    build = 'build';
 
-var src = {
-    sass: 'sass/main.sass/',
-    js: 'js/index.js/',
-};
+// HTML processing
+function html(){
+    return gulp.src(src + '/html/**/*')
+        .pipe(newer(build))
+        .pipe(devBuild ? noop() : htmlclean())
+        .pipe(gulp.dest(build))
+        .pipe(sync.stream());
+}
+// CSS processing
+function css(){
+    const out = build + '/assets/css'
 
-var output = {
-    css: 'build/main.css/',
-    js: 'build/main.js/',
-};
+    return gulp.src(src + '/scss/main.scss')
+        .pipe(sass({
+            errorLogToConsole: devBuild,
+            outputStyle: devBuild ? 'expanded' : 'compressed'
+        })).on('error', sass.logError)
+        .pipe(concat('bundle.min.css'))
+        .pipe(gulp.dest(out))
+        .pipe(sync.stream());
+}
+// JavaScript processing
+function js(){
+    const out = build + '/assets/js'
 
-gulp.task('css', function () {
-    return gulp.src(src.sass + 'main.scss')
-        .pipe(sass())
-        .pipe(autoprefix('last 10 version'))
-        .pipe(minify_css())
-        .pipe(rename({basename: 'styles'}))
-        .pipe(gulp.dest(output.css))
-        .pipe(notify('CSS processed.'))
-        .pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('js', function () {
     return gulp.src([
-            src.js + 'main.js',
-        ])
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(output.js))
-        .pipe(notify('JS processed.'));
-});
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/parsleyjs/dist/parsley.js',
+        src + '/js/**/*'
+    ])
+    .pipe(newer(out))
+    .pipe(concat('bundle.min.js'))
+    .pipe(devBuild ? noop() : terser())
+    .pipe(gulp.dest(out))
+    .pipe(sync.stream());
+}
+/**
+ * Watch for file changes
+ * 
+ * @param {*} done 
+ * @return void
+ */
+function watch(done){
+    sync.init({
+        server: {
+            baseDir: './' + build
+        }
+    });
 
-gulp.task('watch', function () {
-    browserSync.init({server: './output'});
-    gulp.watch(src.sass + '**/*.scss', ['css']);
-    gulp.watch(src.js + '**/*.js', ['js']);
-    gulp.watch(src.js + '**/*.js', ['js']);
-});
+    // HTML changes
+    gulp.watch(src + '/html/**/*', html);
+    // CSS changes
+    gulp.watch(src + '/scss/**/*', css);
+    // JS changes
+    gulp.watch(src + '/js/**/*', js);
+    // Reload browser
+    sync.reload();
 
-gulp.task('default', ['css', 'js', 'watch']);
+    done();
+}
+
+// Create gulp single tasks
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.watch = watch;
+
+// Run all tasks
+exports.build = gulp.parallel(exports.html, exports.css, exports.js);
+
+// Default task
+exports.default = gulp.series(exports.build, exports.watch);
