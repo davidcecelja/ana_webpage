84c90ac3befa066c8ce90ab0c319f85919aeb24e
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2020-03-06 00:15:42 +0100

    add geolocation

---
+++ src/js/base/jquery.js
@@ -0,0 +1,30 @@
+(function($){
+    // Space for custom jQuery functions
+
+    $.fn.getFormData = function(){
+        let data = {};
+        let dataArray = $(this).serializeArray();
+
+        for(let i=0; i < dataArray.length; i++){
+            data[dataArray[i].name] = dataArray[i].value;
+        }
+
+        return data;
+    }
+
+}) (jQuery);
+
+let getLoader = function(){
+    let html = '';
+        html += '<div class="loader">';
+        html += '<img src="/assets/images/loader.svg">';
+        html += '</div>';
+
+    return html;
+}
+
+let setAjaxMessage = function(message){
+    let html = '<div class="message">'+ message +'</div>';
+
+    return html;
+}
